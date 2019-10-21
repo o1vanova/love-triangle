f873d5a21060afb004fe7a8c2024284a72a8f88a
Olga Ivanova <olgai1409@gmail.com>
2019-10-21 13:36:21 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  let i = 1;
+
+  while(i < preferences.length + 1) {
+
+    let first = preferences[i - 1];
+    let second = preferences[first - 1];
+    let third = preferences[second - 1];
+
+    count += i === third && first !== second && second !== third;
+    i++;
+  }
+  return count/3;
 };
